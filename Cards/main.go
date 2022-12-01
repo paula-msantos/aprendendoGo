ff25601324213238ae2ff777268f28d4e538fb96
Paula Líbia Santos <paula.msantos@mercadolivre.com>
2022-12-01 15:21:15 -0300

    variáveis

---
+++ Cards/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades" //para inicializar uma nova variavel, nao pode ser usado quando passamos outro valor.
+	fmt.Println(card)
+}
